23518b4d6cc0c79e55364671eb6ffdfc24af48fb
Matthew Holt <mholt@users.noreply.github.com>
2019-10-21 11:48:22 -0600

    ratelimiter: Add Window() and MaxEvents() methods; handle 0 values

--- ratelimiter.go
+++ ratelimiter.go
@@ -5,9 +5,17 @@ import (
 	"time"
 )
 
-// NewRateLimiter returns a rate limiter that allows up to
-// maxEvents in a sliding window of size window.
+// NewRateLimiter returns a rate limiter that allows up to maxEvents
+// in a sliding window of size window. If maxEvents and window are
+// both 0, or if maxEvents is non-zero and window is 0, rate limiting
+// is disabled. If maxEvents is 0 but the window is non-zero, it is
+// impossible to make reservations, so Allow() will always return false
+// and Wait() will panic (instead of blocking forever). This function
+// panics if maxEvents is less than 0.
 func NewRateLimiter(maxEvents int, window time.Duration) *RingBufferRateLimiter {
+	if maxEvents < 0 {
+		panic("maxEvents cannot be less than zero")
+	}
 	return &RingBufferRateLimiter{
 		window: window,
 		ring:   make([]time.Time, maxEvents),
@@ -20,8 +28,8 @@ func NewRateLimiter(maxEvents int, window time.Duration) *RingBufferRateLimiter 
 // not valid; use NewRateLimiter to get one.
 type RingBufferRateLimiter struct {
 	window time.Duration
-	ring   []time.Time
-	cursor int // always points to the oldest timestamp
+	ring   []time.Time // maxEvents == len(ring)
+	cursor int         // always points to the oldest timestamp
 	mu     sync.Mutex
 }
 
@@ -31,6 +39,9 @@ type RingBufferRateLimiter struct {
 func (r *RingBufferRateLimiter) Allow() bool {
 	r.mu.Lock()
 	defer r.mu.Unlock()
+	if len(r.ring) == 0 {
+		return r.window == 0
+	}
 	if time.Since(r.ring[r.cursor]) >= r.window {
 		r.reserve(time.Now())
 		return true
@@ -39,9 +50,18 @@ func (r *RingBufferRateLimiter) Allow() bool {
 }
 
 // Wait makes a reservation then blocks until the
-// event is allowed to occur.
+// event is allowed to occur. It panics if maxEvents
+// is 0 but the window is non-zero, because Wait
+// would only be able to block forever in that case.
 func (r *RingBufferRateLimiter) Wait() {
 	r.mu.Lock()
+	if len(r.ring) == 0 {
+		if r.window == 0 {
+			r.mu.Unlock()
+			return
+		}
+		panic("cannot wait when maxEvents = 0 and window != 0 ")
+	}
 	if r.ring[r.cursor].IsZero() {
 		r.reserve(time.Now())
 		r.mu.Unlock()
@@ -53,6 +73,14 @@ func (r *RingBufferRateLimiter) Wait() {
 	time.Sleep(time.Until(then))
 }
 
+// MaxEvents returns the maximum number of events that
+// are allowed within the sliding window.
+func (r *RingBufferRateLimiter) MaxEvents() int {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return len(r.ring)
+}
+
 // SetMaxEvents changes the maximum number of events that are
 // allowed in the sliding window. If the new limit is lower,
 // the oldest events will be forgotten. If the new limit is
@@ -62,6 +90,11 @@ func (r *RingBufferRateLimiter) SetMaxEvents(maxEvents int) {
 	newRing := make([]time.Time, maxEvents)
 	r.mu.Lock()
 	defer r.mu.Unlock()
+
+	// only make the change if the new limit is different
+	if maxEvents == len(r.ring) {
+		return
+	}
 
 	// the new ring may be smaller; fast-forward to the
 	// oldest timestamp that will be kept in the new
@@ -88,6 +121,13 @@ func (r *RingBufferRateLimiter) SetMaxEvents(maxEvents int) {
 
 	r.ring = newRing
 	r.cursor = 0
+}
+
+// Window returns the size of the sliding window.
+func (r *RingBufferRateLimiter) Window() time.Duration {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.window
 }
 
 // SetWindow changes r's sliding window duration to window.
