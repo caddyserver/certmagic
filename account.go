97a23b904c7e6dcf820b4aa381eebb1ab8873ef3
Andreas Kohn <andreas.kohn@framer.com>
2024-04-18 20:08:56 +0200

    `continue` when we cannot load the private key for an account
    Not being able to load this might be caused by a storage problem, or it could have been something
    we did earlier. In either case we do not know whether this is the account we're looking for, and breaking
    out now will trigger expensive calls to the ACME server to lookup the account and then save that account
    again even though it was perfectly fine to begin with.

--- account.go
+++ account.go
@@ -90,9 +90,8 @@ func (*ACMEIssuer) newAccount(email string) (acme.Account, error) {
 func (am *ACMEIssuer) GetAccount(ctx context.Context, privateKeyPEM []byte) (acme.Account, error) {
 	email := am.getEmail()
 	if email == "" {
-		account, err := am.loadAccountByKey(ctx, privateKeyPEM)
-		if err == nil || !errors.Is(err, fs.ErrNotExist) {
-			return account, err
+		if account, err := am.loadAccountByKey(ctx, privateKeyPEM); err == nil {
+			return account, nil
 		}
 	} else {
 		keyBytes, err := am.config.Storage.Load(ctx, am.storageKeyUserPrivateKey(am.CA, email))
@@ -115,9 +114,14 @@ func (am *ACMEIssuer) loadAccountByKey(ctx context.Context, privateKeyPEM []byte
 		email := path.Base(accountFolderKey)
 		keyBytes, err := am.config.Storage.Load(ctx, am.storageKeyUserPrivateKey(am.CA, email))
 		if err != nil {
-			return acme.Account{}, err
+			// Try the next account: This one is missing its private key, if it turns out to be the one we're looking
+			// for we will try to save it again after confirming with the ACME server.
+			continue
 		}
 		if bytes.Equal(bytes.TrimSpace(keyBytes), bytes.TrimSpace(privateKeyPEM)) {
+			// Found the account with the correct private key, try loading it. If this fails we we will follow
+			// the same procedure as if the private key was not found and confirm with the ACME server before saving
+			// it again.
 			return am.loadAccount(ctx, am.CA, email)
 		}
 	}
