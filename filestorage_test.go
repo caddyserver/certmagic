fddc09b0f5ff48fcd354dcfafcc8afcb9f5d5e90
a <a@tuxpa.in>
2024-07-06 18:29:43 -0500

    noot

---
+++ filestorage_test.go
@@ -0,0 +1,79 @@
+package certmagic_test
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"testing"
+
+	"github.com/caddyserver/certmagic"
+	"github.com/stretchr/testify/require"
+)
+
+
+
+func TestFileStorageStoreLoad(t *testing.T) {
+	ctx := context.Background()
+	tmpDir, err := os.MkdirTemp(os.TempDir(), "certmagic*")
+	require.NoError(t,err,"allocating tmp dir")
+	defer os.RemoveAll(tmpDir)
+	s := &certmagic.FileStorage{
+		Path: tmpDir,
+	}
+	err = s.Store(ctx, "foo", []byte("bar"))
+	require.NoError(t,err)
+	dat, err := s.Load(ctx, "foo")
+	require.NoError(t,err)
+	require.EqualValues(t, dat, []byte("bar"))
+}
+
+func TestFileStorageStoreLoadRace(t *testing.T) {
+	ctx := context.Background()
+	tmpDir, err := os.MkdirTemp(os.TempDir(), "certmagic*")
+	require.NoError(t,err,"allocating tmp dir")
+	defer os.RemoveAll(tmpDir)
+	s := &certmagic.FileStorage{
+		Path: tmpDir,
+	}
+	a := bytes.Repeat([]byte("a"), 4096 * 1024)
+	b := bytes.Repeat([]byte("b"), 4096 * 1024)
+	err = s.Store(ctx, "foo", a)
+	require.NoError(t,err)
+	go func() {
+		err = s.Store(ctx, "foo", b)
+		require.NoError(t,err)
+	}()
+	dat, err := s.Load(ctx, "foo")
+	require.NoError(t,err)
+	require.Len(t, dat, 4096 * 8)
+}
+
+
+
+func TestFileStorageWriteLock(t *testing.T) {
+	ctx := context.Background()
+	tmpDir, err := os.MkdirTemp(os.TempDir(), "certmagic*")
+	require.NoError(t,err,"allocating tmp dir")
+	defer os.RemoveAll(tmpDir)
+	s := &certmagic.FileStorage{
+		Path: tmpDir,
+	}
+	// cctx is a cancelled ctx. so if we can't immediately get the lock, it will fail
+	cctx, cn := context.WithCancel(ctx)
+	cn()
+	// should success
+	err = s.Lock(cctx, "foo")
+	require.NoError(t,err)
+	// should fail
+	err = s.Lock(cctx, "foo")
+	require.Error(t,err)
+
+	err = s.Unlock(cctx, "foo")
+	require.NoError(t,err)
+	// shouldn't fail
+	err = s.Lock(cctx, "foo")
+	require.NoError(t,err)
+
+	err = s.Unlock(cctx, "foo")
+	require.NoError(t,err)
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/libdns/libdns v0.2.2
 	github.com/mholt/acmez/v2 v2.0.1
 	github.com/miekg/dns v1.1.59
+	github.com/stretchr/testify v1.8.1
 	github.com/zeebo/blake3 v0.2.3
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.23.0
@@ -17,10 +18,13 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/tools v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
