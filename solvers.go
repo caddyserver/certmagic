4293198e094ded561f69e2fc3df49d53c3c5cb89
Matthew Holt <mholt@users.noreply.github.com>
2024-11-05 11:02:47 -0700

    Allow empty DNS record names (fix #314)

--- solvers.go
+++ solvers.go
@@ -547,14 +547,15 @@ func (s *DNSManager) getDNSPresentMemory(dnsName, recType, value string) (dnsPre
 	defer s.recordsMu.Unlock()
 
 	var memory dnsPresentMemory
+	var found bool
 	for _, mem := range s.records[dnsName] {
 		if mem.zoneRec.record.Type == recType && mem.zoneRec.record.Value == value {
 			memory = mem
+			found = true
 			break
 		}
 	}
-
-	if memory.zoneRec.record.Name == "" {
+	if !found {
 		return dnsPresentMemory{}, fmt.Errorf("no memory of presenting a DNS record for %q (usually OK if presenting also failed)", dnsName)
 	}
 
